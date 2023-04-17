5462075720cdeadb974a1b6e32c3d11ba0c70761
DarkSky3309 <lust3309@gmail.com>
2023-04-17 22:06:51 +0300

    improve animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,12 @@
 import React, {useEffect, useState} from 'react';
+import Profession from "./component/Profession/Profession";
 
 const Header = () => {
-    const [profession, setProfession] = useState<string>('');
-    const listOfProfession = [
-        'Front-end developer',
-        'UI-designer',
-        'Freelancer',
-    ]
-    const [currentWordIndex, setCurrentWordIndex] = useState(0);
-    const timer = (ms:number) => new Promise(res => setTimeout(res, ms))
-    async function writeProfession (index: number) {
-        for (const word of listOfProfession[index]) {
-            setProfession(prevState => prevState + word)
-            await timer(50);
-        }
-        await timer(2000)
-        for (let i = listOfProfession[currentWordIndex].length; i > 0; i--){
-            setProfession(prevState => prevState.slice(0, -1))
-            await timer(50)
-        }
-    }
-    useEffect(() => {
-        if (currentWordIndex < listOfProfession.length){
-            writeProfession(currentWordIndex).then(() => setCurrentWordIndex(prevState => prevState + 1))
-        } else {
-            setCurrentWordIndex(0)
-        }
-    }, [currentWordIndex])
+
     return (
         <header>
             <h1>Hi. this is Igor Shevchenko</h1>
-            <p>I'm a {profession}</p>
+            <Profession/>
         </header>
     );
 };
---
+++ src/components/Header/component/Profession/Profession.tsx
@@ -0,0 +1,41 @@
+import React, {useEffect, useRef, useState} from 'react';
+import './Profession.scss'
+const Profession = () => {
+    const [profession, setProfession] = useState<string>('');
+    const listOfProfession = [
+        'Front-end developer',
+        'UI-designer',
+        'Freelancer',
+    ]
+    const [currentWordIndex, setCurrentWordIndex] = useState(0);
+    const blink = useRef() as React.MutableRefObject<HTMLSpanElement>
+    const timer = (ms:number) => new Promise(res => setTimeout(res, ms))
+    async function writeProfession (index: number) {
+        for (const word of listOfProfession[index]) {
+            setProfession(prevState => prevState + word)
+            await timer(50);
+        }
+        blink.current.classList.add('blinking')
+        await timer(2000)
+        blink.current.classList.remove('blinking')
+        for (let i = listOfProfession[currentWordIndex].length; i > 0; i--){
+            setProfession(prevState => prevState.slice(0, -1))
+            await timer(50)
+        }
+    }
+    useEffect(() => {
+        if (currentWordIndex < listOfProfession.length){
+            writeProfession(currentWordIndex).then(() => setCurrentWordIndex(prevState => prevState + 1))
+        } else {
+            setCurrentWordIndex(0)
+        }
+    }, [currentWordIndex])
+    useEffect(() => {
+        setProfession('F')
+    }, [])
+    return (
+        <p>I'm a <span className={'profession'}>{profession}</span><span className={'blinking'} ref={blink}>|</span></p>
+    );
+};
+
+export default Profession;

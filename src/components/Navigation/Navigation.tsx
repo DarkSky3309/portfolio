1c10015b8d7b246739591713959a2924743992ac
DarkSky3309 <lust3309@gmail.com>
2023-04-20 13:08:23 +0300

    designed navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -7,14 +7,14 @@ const Navigation = () => {
             <div className={'aside__info'}>
                 <img src={myPhoto} alt="my photo"/>
                 <h3>I. Shevchenko</h3>
-                <div className="aside__socialMedia">
+                <div>
                     <a href=""><i className="ri-instagram-line"></i></a>
                     <a href=""><i className="ri-telegram-line"></i></a>
                     <a href=""><i className="ri-linkedin-box-line"></i></a>
                 </div>
             </div>
             <nav>
-                <ul>
+                <ul className={"aside__navigation"}>
                     <li><a href=""><i className="ri-home-4-fill"></i>Home</a></li>
                     <li><a href=""><i className="ri-user-fill"></i>About Me</a></li>
                     <li><a href=""><i className="ri-briefcase-4-fill"></i>Portfolio</a></li>

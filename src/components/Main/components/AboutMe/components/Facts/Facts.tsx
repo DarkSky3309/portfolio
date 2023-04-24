886ff751f58901139a63fdcfdc0c5efac7058a5e
DarkSky3309 <lust3309@gmail.com>
2023-04-24 10:20:06 +0300

    create waypoint

--- src/components/Main/components/AboutMe/components/Facts/Facts.tsx
+++ src/components/Main/components/AboutMe/components/Facts/Facts.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import StatisticItem from "./StatisticItem/StatisticItem";
 import './facts.scss'
+import {Waypoint} from "react-waypoint";
 
 const Facts = () => {
+    const waypoint = useRef()
+    const [happyClient, setHappyClient] = useState<string>("14");
+    const [projectCompleted, setProjectCompleted] = useState('15');
+
+    const onEnter = () => {
+    }
+
     return (
         <div className={'aboutMe__facts'}>
             <h3>Facts</h3>
@@ -11,6 +19,7 @@ const Facts = () => {
                 successful website should not only look great, but also be easy to maintain and update, which is why I
                 am committed to producing high-quality and easily maintainable code.</p>
             <div className={'aboutMe__facts-statistic'}>
+                <Waypoint onEnter={onEnter} ref={waypoint}/>
                 <StatisticItem iClas={"ri-emotion-happy-line"} number={"14+"} description={"Happy Client"}/>
                 <StatisticItem iClas={"ri-file-code-line"} number={"20+"} description={"Project Completed"}/>
                 <StatisticItem iClas={"ri-customer-service-2-line"} number={"24/7"} description={"Customer service"}/>
